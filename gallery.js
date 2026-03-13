c67008667112ac1af280f7cc1ec6d009979b6763
Rob Mauceri <rmauceri@outlook.com>
2026-03-13 14:25:16 -0700

    update curated gallery

--- gallery.js
+++ gallery.js
@@ -8,4 +8,315 @@
 //   3. Paste it at the end of this array
 
 const MOKURI_GALLERY_PRESETS = [
+
+  // Crane and Rabbit (600×420)
+  {
+  "elements": [
+    {
+      "id": 1,
+      "defId": "crane",
+      "x": 501.7241379310345,
+      "y": 290.1724137931035,
+      "scaleX": 0.9870689655172414,
+      "scaleY": 0.9870689655172414,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {},
+      "variationSeed": 81411
+    },
+    {
+      "id": 2,
+      "defId": "rabbit",
+      "x": 85.86206896551727,
+      "y": 203.27586206896552,
+      "scaleX": 0.48068370986920356,
+      "scaleY": 0.48068370986920356,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "detail": 4,
+        "body": "#f5f0e1"
+      },
+      "variationSeed": 6105
+    },
+    {
+      "id": 3,
+      "defId": "rock-formation",
+      "x": 156.69222195953932,
+      "y": 329.99999999999994,
+      "scaleX": 0.6546970244374372,
+      "scaleY": 0.6546970244374372,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {},
+      "variationSeed": 99397
+    },
+    {
+      "id": 4,
+      "defId": "stone-path",
+      "x": 113.24597250835893,
+      "y": 258.1034482758621,
+      "scaleX": 1,
+      "scaleY": 1,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "stone": 0,
+        "shadow": 3,
+        "moss": "#2a2a4a"
+      },
+      "variationSeed": 33388
+    },
+    {
+      "id": 5,
+      "defId": "stone-path",
+      "x": 251.26414265932655,
+      "y": 255.86206896551727,
+      "scaleX": 1,
+      "scaleY": -1,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "stone": 2
+      },
+      "variationSeed": 93787,
+      "locked": false
+    },
+    {
+      "id": 6,
+      "defId": "bamboo-grove",
+      "x": 396.2068965517242,
+      "y": 335.1724137931035,
+      "scaleX": 0.4342672413793103,
+      "scaleY": 0.4342672413793103,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "detail": 2
+      },
+      "variationSeed": 93900
+    },
+    {
+      "id": 7,
+      "defId": "bamboo-grove",
+      "x": 351.17241379310354,
+      "y": 357.75862068965523,
+      "scaleX": 0.32008490636147446,
+      "scaleY": 0.32008490636147446,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "detail": 2
+      },
+      "variationSeed": 45289,
+      "locked": false
+    },
+    {
+      "id": 8,
+      "defId": "bamboo-grove",
+      "x": 521.0344827586207,
+      "y": 368.96551724137936,
+      "scaleX": 0.2635181772200069,
+      "scaleY": 0.2635181772200069,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "detail": 2
+      },
+      "variationSeed": 21904,
+      "locked": false
+    },
+    {
+      "id": 9,
+      "defId": "cloud-bank",
+      "x": 407.64294640143197,
+      "y": 62.58620689655169,
+      "scaleX": 0.5209430558673533,
+      "scaleY": 0.5209430558673533,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "body": "#f5f0e1"
+      },
+      "variationSeed": 20516
+    },
+    {
+      "id": 10,
+      "defId": "cloud-bank",
+      "x": 484.70667702387783,
+      "y": 106.89655172413791,
+      "scaleX": 0.36325147442581823,
+      "scaleY": 0.36325147442581823,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "body": "#c8c8c8",
+        "highlight": 0
+      },
+      "variationSeed": 50190,
+      "locked": false
+    },
+    {
+      "id": 11,
+      "defId": "cloud-bank",
+      "x": 518.3241581951434,
+      "y": 45.689655172413794,
+      "scaleX": 0.2727002088285429,
+      "scaleY": 0.2727002088285429,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "body": "#c8c8c8",
+        "highlight": 0
+      },
+      "variationSeed": 35745,
+      "locked": false
+    },
+    {
+      "id": 12,
+      "defId": "hanko-diamond",
+      "x": 571.052720456618,
+      "y": 391.551724137931,
+      "scaleX": 0.45060623459083327,
+      "scaleY": 0.45060623459083327,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {},
+      "variationSeed": 14145
+    },
+    {
+      "id": 13,
+      "defId": "koi-leaping",
+      "x": 76.03448275862067,
+      "y": 319.65517241379314,
+      "scaleX": 0.33082174465828507,
+      "scaleY": 0.33082174465828507,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "belly": "#8a8aaa",
+        "fin": "#c4b870",
+        "detail": "#1a1a3a"
+      },
+      "variationSeed": 75483
+    }
+  ],
+  "nextId": 14,
+  "paletteId": "edo",
+  "paperW": 600,
+  "paperH": 420,
+  "paperType": "unryu",
+  "inkLoad": "light",
+  "impressions": 1,
+  "sky": "day",
+  "ground": "water",
+  "horizon": 0.62,
+  "mist": 2,
+  "backgroundCarveStrokes": [],
+  "presentationMargin": "standard",
+  "presentationTitle": "",
+  "presentationEdition": "",
+  "presentationHanko": null,
+  "presentationHankoPos": "bottom-right",
+  "presentationDeckle": true,
+  "compositionName": "Crane and Rabbit"
+},
+
+  // Sparrow and Blossums (800×320)
+  {
+  "elements": [
+    {
+      "id": 1,
+      "defId": "distant-village",
+      "x": 316.82410080158274,
+      "y": 86.22346529995372,
+      "scaleX": 1,
+      "scaleY": 1,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "tree": "#e8c870",
+        "detail": "#c23b22"
+      },
+      "variationSeed": 80000
+    },
+    {
+      "id": 2,
+      "defId": "cloud-wisp",
+      "x": 124.4992999747808,
+      "y": 47.27146606112012,
+      "scaleX": 0.6430737276412758,
+      "scaleY": 0.6430737276412758,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "cloud": 3,
+        "detail": "#e8c870"
+      },
+      "variationSeed": 78873
+    },
+    {
+      "id": 3,
+      "defId": "cherry-branch",
+      "x": 668.6078164867548,
+      "y": 101.7652405569981,
+      "scaleX": -1.3166614388118754,
+      "scaleY": 1.3166614388118754,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "blossom": "#d4a030",
+        "branch": 1
+      },
+      "variationSeed": 8598
+    },
+    {
+      "id": 4,
+      "defId": "sparrow",
+      "x": 131.39215712731243,
+      "y": 249.5359452395718,
+      "scaleX": -1,
+      "scaleY": 1,
+      "rotation": -9.458250958410446,
+      "carveLevel": 2,
+      "colorOverrides": {
+        "detail": "#c46a20",
+        "body": 2
+      },
+      "variationSeed": 70098
+    },
+    {
+      "id": 5,
+      "defId": "hanko-mokuri",
+      "x": 774.8862489724844,
+      "y": 296.1237527939734,
+      "scaleX": 0.6178343911553,
+      "scaleY": 0.6178343911553,
+      "rotation": 0,
+      "carveLevel": 2,
+      "colorOverrides": {},
+      "variationSeed": 81746
+    }
+  ],
+  "nextId": 6,
+  "paletteId": "sumi",
+  "paperW": 800,
+  "paperH": 320,
+  "paperType": "hosho",
+  "inkLoad": "standard",
+  "impressions": 1,
+  "sky": "day",
+  "ground": "water",
+  "horizon": 0.33,
+  "mist": 3,
+  "backgroundCarveStrokes": [],
+  "presentationMargin": "wide",
+  "presentationTitle": "",
+  "presentationEdition": "",
+  "presentationHanko": null,
+  "presentationHankoPos": "bottom-right",
+  "presentationDeckle": true,
+  "compositionName": "Sparrow and Blossums"
+},
+
 ];
